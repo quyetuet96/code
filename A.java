ecd076c349eceb6092735217097754a835b3c223
quyet.pv <quyet.pv@isofh.com>
2026-05-16 23:01:23 +0700

    SAKURA4-in SAKURA4

--- A.java
+++ A.java
@@ -10,6 +10,7 @@ public class A {
       System.out.println("hello world");
       System.out.println("hello world tiep");
       System.out.println("hello world tiep nua");
+      System.out.println("SAKURA4");
     }
   }
 }
