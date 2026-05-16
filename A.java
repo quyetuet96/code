442e93f7ab8bd54675c8c98d8d5ddb06ffa70798
quyet.pv <quyet.pv@isofh.com>
2026-05-16 22:41:42 +0700

    SAKURA1-tinh nang 1

--- A.java
+++ A.java
@@ -3,6 +3,8 @@ public class A {
   public static void main(String[] args) {
       int x = 1;
       int y = 2;
-      int z = 3;
+      int z = 5;
+      // them tinh nang SAKURA1
+      // them tinh nang SAKURA1 tiep
   }
 }
