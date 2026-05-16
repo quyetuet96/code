b728365af43300862a3abcc263b5c317ee77714a
quyet.pv <quyet.pv@isofh.com>
2026-05-16 22:47:28 +0700

    SAKURA3-check them dieu kien hello world

--- A.java
+++ A.java
@@ -6,7 +6,7 @@ public class A {
       int z = 5;
       // them tinh nang SAKURA1
       // them tinh nang SAKURA1 tiep
-    if (x == 1) {
+    if (x == 1 && y == 2) {
       System.out.println("hello world");
       System.out.println("hello world tiep");
       System.out.println("hello world tiep nua");
